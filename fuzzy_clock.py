e785b45f9bd28f00081908acca5dfbdaf010d5a8
GMargherini <84579545+GMargherini@users.noreply.github.com>
2023-09-06 11:48:26 +0000

    refactoring

--- fuzzy_clock.py
+++ fuzzy_clock.py
@@ -22,15 +22,25 @@ def main():
     hour_text = hours_dict[language][h_index]
     minute_text = minutes_dict[language][m_index]
     if minutes > 34+2.5:
-        hour_text = hours_dict[language][h_index+1]
-        if hours == 11 or hours == 12:
-            hour_text = hours_dict[language][13]
+        hour_text = hours_dict[language][(h_index+1)%12]
+        if hours == 11:
+            hour_text = hours_dict[language][12]
+    elif hours == 12:
+        hour_text = hours_dict[language][12]
+    print_time(language,hour_text,minute_text)
+ 
+def print_time(language, hour_text, minute_text):
     match language:
         case 'it': print(f'{hour_text} {minute_text}')
         case 'en': print(f'{minute_text} {hour_text}')
 
 def print_help():
-    print(f'Available languages: {languages}')
+    print('Usage: python fuzzy_clock.py [OPTIONS] [LANGUAGE]')
+    print('[OPTIONS]:\t-h:\tshow this help screen')
+    print(f'[LANGUAGE]:\t{languages[0]}')
+    for l in languages: 
+        if not l == languages[0]:
+            print(f'\t\t{l}') 
     sys.exit()
 
 if __name__ == '__main__':
