7c736687bb3f7a514ced6d06d86200d1e1fa51e1
Gmargherini <giorgio.margherini@gmail.com>
2024-08-02 20:32:30 +0100

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,14 @@ use std::env;
 use chrono::Timelike;
 
 struct Time {
-    h:u32, 
-    m:f32,
+    h:u64, 
+    m:f64,
 }
 
 impl Time {
     fn get_time_string(&self, lan:&str, nl:bool) -> String{
-        let (hours, mins) = Time::get_language(lan);
-        let m_ind = (((self.m+2.5)) /5.0) as usize;
-        let h_ind = if m_ind > 6 {
-            if self.h == 23{
-                0
-            } else {
-                ((self.h%12) as usize)%12 + 1
-            }
-        } else {
-            (self.h%12) as usize
-        };
+        let (hours, mins) = Self::get_language(lan);
+        let (h_ind, m_ind) = self.get_indexes(); 
 
         let mut ht = if h_ind == 0 && (self.h == 11 || self.h == 12){
             hours[12].to_owned()
@@ -29,36 +20,37 @@ impl Time {
 
         let mut mt = mins[m_ind].to_owned();
         if m_ind == 0{
-            let res = format!("{mt}{ht}").to_owned();
-            return res
+            return format!("{ht}").to_owned();
         }
 
-        let res;
-        if nl{
-            let hl = ht.len();
-            let ml = mt.len();
-            if hl > ml{
-                let diff = (hl - ml)/2;
-                let new = " ".repeat(diff) + &mt;
-                mt = new;
+        let separator = if nl {(ht,mt) = Self::update_text(ht,mt); "\n"} else {" "};
+        return match lan {
+            "it" => {format!("{ht}{separator}{mt}").to_owned()}
+            "en"|_ => format!("{mt}{separator}{ht}").to_owned(),
+        };
+    }
+
+    fn get_indexes(&self) -> (usize, usize){
+        let m_ind = ((((self.m+2.5)) / 5.0) as usize)%12;
+        let h_ind = if self.m > 30.0 {
+            if self.h == 23{
+                0
+            } else {
+                ((self.h%12) as usize)%12 + 1
             }
-            else if ml> hl {
-                let diff = (ml - hl)/2;
-                let new = " ".repeat(diff) + &ht;
-                ht = new;
-            }
-            res = match lan {
-                "it" => {format!("{ht}\n{mt}").to_owned()}
-                "en"|_ => format!("{mt}\n{ht}").to_owned(),
-            };
-        }
-        else{
-            res = match lan {
-                "it" => {format!("{ht} {mt}").to_owned()}
-                "en"|_ => format!("{mt} {ht}").to_owned(),
-            };
-        }
-        return res
+        } else {
+            (self.h%12) as usize
+        };
+        return (h_ind, m_ind);
+    }
+
+    fn update_text(ht:String, mt:String) -> (String, String) {
+        let hl = ht.len() as isize;
+        let ml = mt.len() as isize;
+        
+        let diff = (isize::abs(hl - ml)/2) as usize;
+        let offset = " ".repeat(diff);
+        return if hl > ml {(ht, offset + &mt)} else {(offset + &ht, mt)}
     }
 
     fn get_language(lan:&str) -> ([&'static str;13],[&'static str;12]){
@@ -76,7 +68,8 @@ impl Time {
 
 fn main() {
     let now = chrono::offset::Local::now();
-    let time = Time{h: now.hour(), m: now.minute() as f32};
+    let time = Time{h: now.hour() as u64, m: now.minute() as f64};
+//    let time = Time{h: now.hour() as u64, m: 30 as f64};
     let new_line = match env::args().find(|x| x == "t"){
         Some(_) => true,
         None => false
